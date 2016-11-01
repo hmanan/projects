f1000c6b5bd3f559f33f67847ddfb08a7e13eb5f
Harnoor Singh <hnoormanan@gmail.com>
2016-10-31 23:23:43 -0400

    Add files via upload

---
+++ book.js
@@ -0,0 +1,82 @@
+/**
+ * Created by Harnoor on 10/30/2016.
+ */
+
+var mongoose = require('mongoose');
+
+
+// Book Schema
+var bookSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    genre:{
+        type: String,
+        required: true
+    },
+    author:{
+        type: String,
+        required: true
+    },
+    publisher:{
+        type: String,
+
+    },
+    page:{
+        type: String,
+
+    },
+    image_url:{
+        type: String,
+
+    },
+    buy_url:{
+        type: String,
+
+    },
+    create_date:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+var Book = module.exports = mongoose.model('Book', bookSchema);
+
+// Get Books
+
+module.exports.getBooks = function(callback, limit){
+    Book.find(callback).limit(limit);
+}
+
+// Get Book
+
+module.exports.getBookById = function(id, callback){
+    Book.findById(id, callback);
+}
+
+// Add Book
+module.exports.addBook = function(book, callback){
+    Book.create(book, callback);
+}
+
+module.exports.updateBook = function(id, book, options, callback){
+    var query = {_id: id};
+    var update = {
+        title:book.name,
+        genre:book.genre,
+        description:book.description,
+        author:book.author,
+        publisher:book.publisher,
+        pages:book.pages,
+        image_url:book.image_url,
+        buy_url:book.buy_url
+
+    }
+    Genre.findOneAndUpdate(query, update, options, callback);
+}
+// Delete Book
+module.exports.removeBook = function(id, callback){
+    var query = {_id: id};
+    Book.remove(query, callback);
+}
